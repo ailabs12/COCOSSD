979de9ba698bbdf173d165df9e48a812aa980830
Bakoal <bka2@lab04.maas>
2018-11-06 20:33:44 +0300

    6-20-33

---
+++ test.js
@@ -0,0 +1,19 @@
+var cv = require('opencv4nodejs');
+var { classifyImg } = require('./classify_cocossd');
+
+
+//------------------------ДЛЯ ПРИМЕРА------------------------------
+//переводим картинку в base64
+
+// загрузить картинку из файла
+var img = cv.imread('bill.jpg');
+
+// конвертировать Mat в base64, закодированную jpg картинку
+const base64data =  cv.imencode('.jpg', img).toString('base64'); // Выполнить base64 кодирование
+//-----------------------------------------------------------------
+
+
+const buffer = Buffer.from(base64data,'base64'); // записываем закодированную картинку в буфер
+const image = cv.imdecode(buffer); //Картинка сейчас представлена как Mat
+
+console.log(classifyImg(image));
